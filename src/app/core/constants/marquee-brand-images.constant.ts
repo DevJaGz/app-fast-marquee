e086935744270dd862c9bd7bf82609be8ad279b6
DevJaGz <jgomezpersonal7@gmail.com>
2023-11-30 21:34:18 -0500

    feat: remove brands that have background

--- src/app/core/constants/marquee-brand-images.constant.ts
+++ src/app/core/constants/marquee-brand-images.constant.ts
@@ -1,6 +1,4 @@
 export const BrandImages: string[] = [
-  'assets/images/webp/angular.webp',
-  'assets/images/webp/github.webp',
   'assets/images/webp/google.webp',
   'assets/images/webp/imexhs.webp',
   'assets/images/webp/microsoft.webp',
