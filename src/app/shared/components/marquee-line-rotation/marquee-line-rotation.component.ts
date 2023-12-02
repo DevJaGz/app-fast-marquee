b49f46bf2fef05025735ee58b07dbaab221d1423
DevJaGz <jgomezpersonal7@gmail.com>
2023-12-02 01:24:29 -0500

    feat: set mask percentage to the marquee-cards

--- src/app/shared/components/marquee-line-rotation/marquee-line-rotation.component.ts
+++ src/app/shared/components/marquee-line-rotation/marquee-line-rotation.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxFastMarqueeModule } from '@ngx-fast-marquee';
 
 @Component({
   selector: 'app-marquee-line-rotation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxFastMarqueeModule],
   templateUrl: './marquee-line-rotation.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
