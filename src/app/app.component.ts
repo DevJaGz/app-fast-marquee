c6d3a3912cf8a33fdc79955ef409df50de6202e6
DevJaGz <jgomezpersonal7@gmail.com>
2024-12-02 19:21:44 +0100

    refactor: remove waiting time at first paint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WelcomePlaceholderComponent } from './core/components/welcome-placeholder/welcome-placeholder.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WelcomePlaceholderComponent],
+  imports: [CommonModule, RouterOutlet],
   template: `<div class="h-screen bg-zinc-900 text-slate-200">
-    @defer (on timer(2000ms)) {
-      <router-outlet></router-outlet>
-    } @placeholder {
-      <app-welcome-placeholder />
-    }
+    <router-outlet></router-outlet>
   </div>`,
 })
 export class AppComponent {}
