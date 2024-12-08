ca806f1d090ada18b0497fcde0c2fe1ca02456ae
DevJaGz <jgomezpersonal7@gmail.com>
2024-12-08 15:44:24 +0100

    refactor: remove useless code from ngx fast marquee helper

--- projects/ngx-fast-marquee/src/components/helpers/ngx-fast-marquee.helper.ts
+++ projects/ngx-fast-marquee/src/components/helpers/ngx-fast-marquee.helper.ts
@@ -1,7 +1,6 @@
 import { isPlatformServer } from '@angular/common';
 import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { NgxFastMarqueeDuplicationHelper } from './ngx-fast-marquee-duplication.helper';
-import { Direction } from '../../types';
 import { NgxFastMarqueeLayoutHelper } from './ngx-fast-marquee-layout.helper';
 
 @Injectable({ providedIn: 'root' })
@@ -22,43 +21,9 @@ export class NgxFastMarqueeHelper {
   #layoutHelper = inject(NgxFastMarqueeLayoutHelper);
 
   /**
-   * Debounce timer to avoid multiple resize events.
-   */
-  #onResizeDebounceTimer: ReturnType<typeof setTimeout> | null = null;
-
-  /**
    * True if the current application is running on the server, false otherwise.
    */
   get isPlatformServer(): boolean {
     return isPlatformServer(this.#platformId);
-  }
-
-  /**
-   * Duplicate the items of the inner element.
-   * @param innerElement - Element that contains the items.
-   * @param isAutoFill - Whether to fill the marquee with duplicated items.
-   */
-  duplicateItems(params: {
-    marqueeElement: HTMLElement;
-    innerElement: HTMLElement;
-    isAutoFill: boolean;
-    isOverflowing: boolean;
-  }): void {
-    if (params.isAutoFill) {
-      this.#duplicationHelper.duplicateFillingSpace(
-        params.marqueeElement,
-        params.innerElement,
-      );
-      return;
-    }
-    this.#duplicationHelper.duplicateWithoutFillingSpace(params.innerElement);
-  }
-
-  isContentOverflowing(params: {
-    marqueeElement: HTMLElement;
-    innerElement: HTMLElement;
-    direction: Direction;
-  }): boolean {
-    return this.#layoutHelper.isContentOverflowing(params);
   }
 }
